ee172fad79c06be50197bbd7a83449b93c9edb80
Fiama de Capua <42975721+FihCapua@users.noreply.github.com>
2021-06-25 18:06:41 -0300

    edit tasks use setState

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,12 +6,13 @@ import "./Main.css";
 export default class Main extends Component {
   state = {
     newTask: "",
-    tarefas: []
+    tarefas: [],
+    index: -1 // estado de criação de tasks
   };
 
   handleSubmit = (e) => {
     e.preventDefault(); // bloqueia o envio do form
-    const { tarefas } = this.state; // chama o array de tarefas do state
+    const { tarefas, index } = this.state; // chama o array de tarefas do state
     let { newTask } = this.state; // chama a new Task que vai transformar o array de tarefas em string.
     newTask = newTask.trim(); // elimina os espaços do começo e do final do input
 
@@ -19,10 +20,22 @@ export default class Main extends Component {
 
     const newsTasks = [...tarefas]; // aqui todos os valores (resultados) que forem pra o array (que estava vazio) tarefas serão copiados
 
-    this.setState({
-      tarefas: [...newsTasks, newTask], // tarefas serão iguais ao que for inserido nas novas tarefas (...rest copia as tarefas, newTask jogam os resultados das novas tarefas)
-      newTask: ""
-    });
+    // Criando tarefas
+    if (index === -1) {
+      this.setState({
+        tarefas: [...newsTasks, newTask], // tarefas serão iguais ao que for inserido nas novas tarefas (...rest copia as tarefas, newTask jogam os resultados das novas tarefas)
+        newTask: ""
+      });
+    } else {
+      // Editando tarefas
+      newsTasks[index] = newTask;
+
+      // configura o setState e volta pra o local inicial
+      this.setState({
+        tarefas: [...newsTasks],
+        index: -1 // ja editado e ok
+      });
+    }
   };
 
   handleChange = (e) => {
@@ -33,7 +46,13 @@ export default class Main extends Component {
 
   // Manipula os ícones de edit e delete
   handleEdit = (e, index) => {
-    console.log("Edit", index);
+    // ao editar o valor vai pra o input novamente - graças ao -1  la no state
+    const { tarefas } = this.state; // seta(configura) o estado
+
+    this.setState({
+      index, // seleciona o index da tarefa
+      newTask: tarefas[index] // seleciona a tarefa + o index
+    });
   };
 
   handleDelete = (e, index) => {
