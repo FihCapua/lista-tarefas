880425ed59721ed8909c6850714dfebac6987205
Fiama de Capua <42975721+FihCapua@users.noreply.github.com>
2021-06-25 14:05:46 -0300

    map example in ul > li

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { FaPlus } from "react-icons/fa";
+import { FaEdit, FaWindowClose } from "react-icons/fa";
 import "./Main.css";
 
 export default class Main extends Component {
   state = {
-    newTask: ""
+    newTask: "",
+    tarefas: ["fazer Café", "beber Água", "Estudar"]
   };
 
   handleChange = (e) => {
@@ -14,7 +16,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { newTask } = this.state;
+    const { newTask, tarefas } = this.state;
 
     return (
       <div className="main">
@@ -26,6 +28,18 @@ export default class Main extends Component {
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
